5bfeb942682f6f51c9f7be2c2f2b5232d0ef525d
Gary Basin <gbasin@gmail.com>
2023-05-14 16:24:04 -0400

    Added try/catches

--- src/main.py
+++ src/main.py
@@ -19,8 +19,12 @@ Output: [{"action": "QUERY", "command": "Find all people older than 20."}]
 
 Input: "{command}"
 """
-        classified_command = chat_completion(prompt)
-        return json.loads(classified_command)
+        try:
+            classified_command = chat_completion(prompt)
+            return json.loads(classified_command)
+        except Exception as e:
+            print(f"Error while classifying command: {e}")
+            return []
 
     def process_insert(self, command):
         # Construct a prompt to instruct the LLM to extract data from the insert command into JSON format
@@ -33,8 +37,12 @@ Output: {"name": "John Doe", "age": 25}
 
 Input: "{command}"
 """
-        processed_command = chat_completion(prompt)
-        return json.loads(processed_command)
+        try:
+            processed_insert = chat_completion(prompt)
+            return json.loads(processed_insert)
+        except Exception as e:
+            print(f"Error while processing insert command: {e}")
+            return {}
 
     def process_query(self, json_entry, query):
         # Construct a prompt to instruct the LLM to compare the JSON entry with the query criteria
@@ -49,20 +57,31 @@ Output: Yes
 Database Entry: {json_entry}
 Query: {query}
 """
-        processed_query = chat_completion(prompt)
-        return processed_query.lower() == 'yes'
+        try:
+            processed_query = chat_completion(prompt)
+            return processed_query.lower() == 'yes'
+        except Exception as e:
+            print(f"Error while processing query: {e}")
+            return False
 
 class DatabaseManager:
     def __init__(self, filename):
         self.filename = filename
 
     def insert_data(self, data):
-        with open(self.filename, 'a') as f:
-            f.write(json.dumps(data) + '\n')
+        try:
+            with open(self.filename, 'a') as f:
+                f.write(json.dumps(data) + '\n')
+        except Exception as e:
+            print(f"Error while inserting data: {e}")
 
     def retrieve_data(self):
-        with open(self.filename, 'r') as f:
-            return [json.loads(line) for line in f.readlines()]
+        try:
+            with open(self.filename, 'r') as f:
+                return [json.loads(line) for line in f.readlines()]
+        except Exception as e:
+            print(f"Error while retrieving data: {e}")
+            return []
 
 class CommandProcessor:
     def __init__(self, llm, db_manager):
@@ -70,21 +89,25 @@ class CommandProcessor:
         self.db_manager = db_manager
 
     def handle_command(self, command):
-        classified_commands = self.llm.classify_command(command)
-        results = []
-        for classified_command in classified_commands:
-            if classified_command['action'] == 'INSERT':
-                data = self.llm.process_insert(classified_command['command'])
-                self.db_manager.insert_data(data)
-                results.append(data)
-            elif classified_command['action'] == 'QUERY':
-                data = self.db_manager.retrieve_data()
-                query_results = []
-                for entry in data:
-                    if self.llm.process_query(json.dumps(entry), classified_command['command']):
-                        query_results.append(entry)
-                results.append(query_results)
-        return results
+        try:
+            classified_commands = self.llm.classify_command(command)
+            results = []
+            for classified_command in classified_commands:
+                if classified_command['action'] == 'INSERT':
+                    data = self.llm.process_insert(classified_command['command'])
+                    self.db_manager.insert_data(data)
+                    results.append(data)
+                elif classified_command['action'] == 'QUERY':
+                    data = self.db_manager.retrieve_data()
+                    query_results = []
+                    for entry in data:
+                        if self.llm.process_query(json.dumps(entry), classified_command['command']):
+                            query_results.append(entry)
+                    results.append(query_results)
+            return results
+        except Exception as e:
+            print(f"Error while handling command: {e}")
+            return []
 
 def chat_completion(prompt):
     # This function will call the LLM API with the given prompt and return the response
@@ -96,8 +119,11 @@ def main():
     db_manager = DatabaseManager("../data/database.txt")
     command_processor = CommandProcessor(llm, db_manager)
 
-    results = command_processor.handle_command(' '.join(sys.argv[1:]))
-    print(results)
+    try:
+        results = command_processor.handle_command(' '.join(sys.argv[1:]))
+        print(results)
+    except Exception as e:
+        print(f"An error occurred: {e}")
     
 
 if __name__ == "__main__":
